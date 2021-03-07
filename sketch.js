01cdc70f96b5a35ef8421e8c974622ffafa146ca
yashuwhitehat <79188741+yashuwhitehat@users.noreply.github.com>
2021-03-07 22:08:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-        wall1=createSprite(600,100,30,100);;
+        wall1=createSprite(410,600,100,20);;
 	wall2=createSprite(510,610,20,100);
 	wall3=createSprite(310,610,20,100);
 	engine = Engine.create();
